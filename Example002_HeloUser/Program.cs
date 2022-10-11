8c325d57f9478eb0c2316b195328c4f26f7e1acd
Alexandr1963nt <Alexander.Trofimenko@asapcg.com>
2022-10-11 21:23:10 +0300

    Добавлена Example002_HeloUser

---
+++ Example002_HeloUser/Program.cs
@@ -0,0 +1,15 @@
+﻿//Console.WriteLine("Представьтесь пожалуйста");
+
+//string username = Console.ReadLine();
+
+//Console.WriteLine("Здравствуйте, " + username);
+
+//Console.WriteLine(username);
+
+Console.Write("Представьтесь пожалуйста - ");
+
+string username = Console.ReadLine();
+
+Console.Write("Здравствуйте, ");
+
+Console.Write(username);
