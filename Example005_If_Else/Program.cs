0eb5d5defce29e32d4a0f2e24b3958460143743b
Alexandr1963nt <Alexander.Trofimenko@asapcg.com>
2022-10-11 22:24:38 +0300

    добавили Прогу на If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Представьтесь пожалуйста - ");
+
+string username = Console.ReadLine();
+
+if (username.ToLower() == "александр")
+{
+    Console.Write("Здравствуйте, Уважаемый ");
+    Console.Write(username);
+}
+else
+{
+    Console.WriteLine("Hi! "+ username);
+}
