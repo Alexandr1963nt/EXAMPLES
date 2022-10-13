cf03e8f7122bf04db1ac963e1bdaf0eabc0d023a
Alexandr1963nt <Alexander.Trofimenko@asapcg.com>
2022-10-13 19:29:49 +0300

    Зачем-то закоммитил ))

--- Example002_HeloUser/Program.cs
+++ Example002_HeloUser/Program.cs
@@ -1,12 +1,4 @@
-﻿//Console.WriteLine("Представьтесь пожалуйста");
-
-//string username = Console.ReadLine();
-
-//Console.WriteLine("Здравствуйте, " + username);
-
-//Console.WriteLine(username);
-
-Console.Write("Представьтесь пожалуйста - ");
+﻿Console.Write("Представьтесь пожалуйста - ");
 
 string username = Console.ReadLine();
 
--- Example005_If_Else/Program.cs
+++ Example005_If_Else/Program.cs
@@ -11,4 +11,3 @@ else
 {
     Console.WriteLine("Hi! "+ username);
 }
-//коммент
